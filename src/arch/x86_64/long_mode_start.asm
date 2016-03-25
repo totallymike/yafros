edd48f5fc25111ed993ecf0606c5b49f5eabf361
Michael Westbom <michael@agilion.com>
2016-03-25 09:28:25 -0400

    Drop more code that we never reach

--- src/arch/x86_64/long_mode_start.asm
+++ src/arch/x86_64/long_mode_start.asm
@@ -6,7 +6,6 @@ long_mode_start:
 extern rust_main
   call rust_main
 
-  ; Print OKAY to the screen
-  mov rax, 0x2f592f412f4b2f4f
-  mov qword [0xb8000], rax
+  ; Should never get here,
+  ; but halt if we do
   hlt
