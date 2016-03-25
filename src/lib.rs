9ccb09f4495283a463698621d633af84bba8d7ae
Michael Westbom <michael@agilion.com>
2016-03-25 09:17:31 -0400

    Add a bunch of debug output

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,26 @@ pub extern fn rust_main(multiboot_information_address: usize) {
   for area in memory_map_tag.memory_areas() {
     println!("    start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
   }
+
+  let elf_sections_tag = boot_info.elf_sections_tag()
+    .expect("Elf sections tag required");
+
+  println!("Kernel sections:");
+  for section in elf_sections_tag.sections() {
+    println!("    addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+             section.addr, section.size, section.flags);
+  }
+
+  let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+    .min().unwrap();
+  let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
+    .max().unwrap();
+
+  let multiboot_start = multiboot_information_address;
+  let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+  println!("kernel_start: 0x{:x}, kernel_end: 0x{:x}", kernel_start, kernel_end);
+  println!("multiboot_start: 0x{:x}, multiboot_end: 0x{:x}", multiboot_start, multiboot_end);
   loop{}
 }
 
